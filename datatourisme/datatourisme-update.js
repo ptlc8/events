588466e0e459d7024a4e997ca226452d26d422d8
Ambi <ptlc.ptlc.ptlc@gmail.com>
2023-04-18 10:56:05 +0300

    new categories + better importation from datatourisme

--- datatourisme/datatourisme-update.js
+++ datatourisme/datatourisme-update.js
@@ -3,7 +3,7 @@ const unzip = require("unzipper");
 const mysql = require("mysql2");
 const credentials = require("./credentials.json");
 
-loadDatatourismeZip(require("fs").createReadStream("flux-15996-202303180145.zip"));
+loadDatatourismeZip(require("fs").createReadStream("flux-15996-202304171803.zip"));
 //loadDatatourismeZipFromUrl(credentials.DATATOURISME_GET_URL);
 
 function loadDatatourismeZipFromUrl(url) {
@@ -53,15 +53,18 @@ async function parseJSONFromStream(stream) {
 }
 
 function parseEvent(datatourismeEvent) {
+    var address = datatourismeEvent.isLocatedAt[0]["schema:address"][0];
+    var title = datatourismeEvent["rdfs:label"].fr[0];
+    var description = (datatourismeEvent.hasDescription?.[0]?.["dc:description"] || datatourismeEvent["rdfs:comment"])?.fr?.[0] ?? "";
     return {
-        id: "DT" + datatourismeEvent["dc:identifier"].substring(0,22),
-        title: datatourismeEvent["rdfs:label"].fr[0],
+        id: "DT" + datatourismeEvent["dc:identifier"].substring(0, 22),
+        title,
         author: datatourismeEvent.hasBeenCreatedBy["schema:legalName"],
-        description: (datatourismeEvent.hasDescription?.[0]?.["dc:description"] || datatourismeEvent["rdfs:comment"])?.fr?.[0] ?? "",
+        description,
         datetime: datatourismeEvent.takesPlaceAt.map(t => t.startDate + " " + (t.startTime || "")),
         endDatetime: datatourismeEvent.takesPlaceAt.map(t => (t.endDate || t.startDate) + " " + (t.endTime || "")),
-        placename: "TODO",
-        categories: datatourismeEvent["@type"].concat(datatourismeEvent.hasTheme?.map(theme => theme["@id"]) ?? []),
+        placename: address["schema:addressLocality"] + ", " + address["schema:postalCode"] + ", " + address["schema:streetAddress"],
+        categories: findCategories(datatourismeEvent["@type"].concat(datatourismeEvent.hasTheme?.map(theme => theme["@id"]) ?? []), title, description),
         public: 1,
         lng: datatourismeEvent.isLocatedAt[0]["schema:geo"]["schema:longitude"],
         lat: datatourismeEvent.isLocatedAt[0]["schema:geo"]["schema:latitude"],
@@ -114,4 +117,62 @@ function updateDatabase(index, events) {
         }
     }
     if (count == 0) console.log("Updating database done (0 events)");
+}
+
+// https://gitlab.adullact.net/adntourisme/datatourisme/ontology/-/blob/master/thesaurus/thesaurus.ttl
+var CATEGORIES = {
+    "party": ["party", "fête"],
+    "arts": [" arts", "artist", " art ", "artsandcraft", "visualart"],
+    "theater": ["théâtr", "theater", "théatr", "comédien"],
+    "music": ["musique","music","concert"],
+    "online": ["en ligne","online"],
+    "children": ["enfants", "tout public", "en famille", "children", "in family", "all age", "accessible à tous"],
+    "exhibition": ["exposition", "expo"],
+    "shopping": ["shopping"],
+    "cinema": ["cinéma", "cinema", "film"],
+    "food": ["nourriture", "alimentair", "food", "restauration", "restaurant", "gastronomie", "gastronomy", "cuisine", "food", "eat", "manger", "déguster", "dégustation", "restaur"],
+    "wellbeing": ["bien-être", "wellbeing", "well-being", "well being", "relaxation", "yoga", "méditation"],
+    "show": ["show", "spectacle", "scène"],
+    "sport": ["sport", "athléti", "athleti", "basketball", "baseball", "football", "soccer", "rugby", "tennis", "volleyball", "handball", "golf", "gym", "hockey", "judo", "karate", "karaté", "natation", "swim", "ski"],
+    "literature": ["literature", "littérature", " livre", " book", "lecture", " read", "écrire", "écrivain", " bd "],
+    "drink": ["boire", "boisson", "drink", "alcool", "alcohol", "bière", "beer", " vin ", " vins ", "wine", "cocktail", "aperitif", "apéritif", "buvette", "café"],
+    "gardening": ["jardine", "jardino", "jardinage", "gardenning", "plant", "flower", "fleur"],
+    "cause": ["cause", "bénévolat", "bénévole", "volontariat", "volontaire", "volunteer", "charité", "charity", " don", "donation", "solidarité", "solidarity", "ngo", "non-profit"],
+    "craft": ["craft", "artisan", "handmade", "hand-made", "hand made", "manufacturé"],
+    "dance": [" dance", "danse", "dancing", "danceevent"],
+    "festival": ["festival"],
+    "garden": ["jardin ", "jardins ", "parc ", "parcs ", "parks ", "garden", "park "],
+    "holiday": ["holiday", "vacance", "vacation"],
+    "market": ["market", "marché", "marchand"],
+    "museum": ["museum", "musée"],
+    "outdoor": ["outdoor", "extérieur", "exterieur", "dehors"],
+    "parade": ["parade", "défilé", "defile"],
+    "religion": ["religious", "religieu", "religion", "spiritual", "catholique", "catholic", "musulman", "muslim", "juif", "jewish", "chrétien", "christian", "église", "church", "mosquée", "mosque", "synagogue", "temple"],
+    "science": ["science", "scientifique", "scientist", "physique", "physic", "math", "informatique", "computer", "technologie", "technology", "astronomie", "astronomy", "biologie", "biology", "chimie", "chemistry", "économie", "economy", "géologie", "geology", "médecine", "medicine", "psychologie", "psychology", "santé", "health", "sociologie", "sociology", "zoologie", "zoology"],
+    "seminar": ["seminar", "séminaire", " conférence", "conference"],
+    "tour": ["tour", "visite", "visite"],
+    "workshop": ["workshop", "atelier", "atelier"],
+    "free": ["free entrance", "gratuit"],
+    "fashion" : ["fashion", "la mode", "vêtement"],
+    "fair": ["fair", "foire"],
+    "videogame": ["gaming", "jeu vid", "jeux vid", "game", "games", "video game", "playstation", "xbox", "nintendo"],
+    "boardgame": ["boardgame", "jeu de société", "jeux de société", "jeu de plateau", "jeux de plateau", "jeu de cartes", "jeux de cartes", "jeu de rôle"],
+};
+function findCategories(dtCategories, title, description) {
+    var result = [];
+    for (let cat in CATEGORIES) {
+        loop: for (let c of CATEGORIES[cat]) {
+            if (title.toLowerCase().includes(c) || description.toLowerCase().includes(c)) {
+                result.push(cat);
+                break loop;
+            }
+            for (let dtCat of dtCategories) {
+                if (dtCat.toLowerCase().includes(c)) {
+                    result.push(cat);
+                    break loop;
+                }
+            }
+        }
+    }
+    return result;
 }
