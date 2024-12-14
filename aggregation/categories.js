2d04d6d36147f4ec5eb6f55da50617de303c035d
Ambi <ptlc.ptlc.ptlc@gmail.com>
2024-12-14 16:35:12 +0100

    aggregation: ajout de quelques mots-clés de catégories + utilisation de regex

--- aggregation/categories.js
+++ aggregation/categories.js
@@ -7,41 +7,41 @@
  */
 const categoriesKeywords = {
     "party": ["party", "fête"],
-    "arts": [" arts", "artist", " art ", "artsandcraft", "visualart"],
-    "theater": ["théâtr", "theater", "théatr", "comédien"],
+    "arts": [" art[s ]", "artist", "artsandcraft", "visualart"],
+    "theater": ["th[ée][âa]te?r", "com[ée]dien"],
     "music": ["musique", "music", "concert"],
     //"online": ["en ligne","online"],
     "children": ["enfants", "tout public", "en famille", "children", "in family", "all age", "accessible à tous"],
     "exhibition": ["exposition", "expo"],
     "shopping": ["shopping"],
-    "cinema": ["cinéma", "cinema", "film"],
-    "food": ["nourriture", "alimentair", "food", "restauration", "restaurant", "gastronomie", "gastronomy", "cuisine", "food", "eat", "manger", "déguster", "dégustation", "restaur"],
-    "wellbeing": ["bien-être", "wellbeing", "well-being", "well being", "relaxation", "yoga", "méditation"],
-    "show": ["show", "spectacle", "scène"],
-    "sport": ["sport", "athléti", "athleti", "basketball", "baseball", "football", "soccer", "rugby", "tennis", "volleyball", "handball", "golf", "gym", "hockey", "judo", "karate", "karaté", "natation", "swim", "ski"],
-    "literature": ["literature", "littérature", " livre", " book", "lecture", " read", "écrire", "écrivain", " bd "],
+    "cinema": ["cin[ée]ma", "film"],
+    "food": ["nourriture", "alimentair", "food", "restaura", "gastronom(ie|y)", "cuisine", "food", "eat", "manger", "d[é|e]gust"],
+    "wellbeing": ["bien[- ]?[êe]tre", "well[- ]?being", "relaxation", "yoga", "m[ée]ditation"],
+    "show": ["show", "spectacle", "sc[èe]ne"],
+    "sport": ["sport", "athl[ée]ti", "(basket|base|foot|volley|hand)ball", "soccer", "rugby", "tennis", "golf", "gym", "hockey", "judo", "karate", "karaté", "natation", "swim", "ski", "vtt", "roller", "kayak"],
+    "literature": ["litt[ée]rature", "litt[ée]raire", " livre", " book", "lecture", " read", "[ée]crire", "[ée]crivain", " bd "],
     "drink": ["boire", "boisson", "drink", "alcool", "alcohol", "bière", "beer", " vin ", " vins ", "wine", "cocktail", "aperitif", "apéritif", "buvette", "café"],
-    "gardening": ["jardine", "jardino", "jardinage", "gardenning", "plant", "flower", "fleur"],
-    "cause": ["cause", "bénévolat", "bénévole", "volontariat", "volontaire", "volunteer", "charité", "charity", " don", "donation", "solidarité", "solidarity", "ngo", "non-profit"],
-    "craft": ["craft", "artisan", "handmade", "hand-made", "hand made", "manufacturé"],
-    "dance": [" dance", "danse", "dancing", "danceevent"],
+    "gardening": ["jardin[eoa]", "gardenning", "plant", "flower", "fleur"],
+    "cause": ["cause", "b[ée]n[ée]vol", "volontai?r", "volunteer", "charit[éey]", "(^| )dons?( |$)", "donation", "solidarit[éy]", " ngo ", "non-profit"],
+    "craft": ["craft", "artisan", "hand[- ]?made", "manufactur"],
+    "dance": [" dan[cs]e", "dancing", "danceevent"],
     "festival": ["festival"],
-    "garden": ["jardin ", "jardins ", "parc ", "parcs ", "parks ", "garden", "park "],
+    "garden": ["jardin ", "jardins ", "par[ck]s? ", "garden"],
     "holiday": ["holiday", "vacance", "vacation"],
     "market": ["market", "marché", "marchand"],
-    "museum": ["museum", "musée"],
-    "outdoor": ["outdoor", "extérieur", "exterieur", "dehors"],
-    "parade": ["parade", "défilé", "defile"],
-    "religion": ["religious", "religieu", "religion", "spiritual", "catholique", "catholic", "musulman", "muslim", "juif", "jewish", "chrétien", "christian", "église", "church", "mosquée", "mosque", "synagogue", "temple"],
-    "science": ["science", "scientifique", "scientist", "physique", "physic", "math", "informatique", "computer", "technologie", "technology", "astronomie", "astronomy", "biologie", "biology", "chimie", "chemistry", "économie", "economy", "géologie", "geology", "médecine", "medicine", "psychologie", "psychology", "santé", "health", "sociologie", "sociology", "zoologie", "zoology"],
-    "seminar": ["seminar", "séminaire", " conférence", "conference"],
-    "tour": ["tour", "visite", "visite"],
-    "workshop": ["workshop", "atelier", "atelier"],
-    "free": ["free entrance", "gratuit"],
-    "fashion": ["fashion", "la mode", "vêtement"],
+    "museum": ["museum", "mus[ée]e", "patrimoine"],
+    "outdoor": ["outdoor", "ext[ée]rieur", "dehors"],
+    "parade": ["parade", "d[ée]fil[ée]"],
+    "religion": ["religious", "religieu", "religion", "spiritual", "catholi(que|c)", "musulman", "muslim", "juif", "jewish", "chr[ée]tien", "christian", "[ée]glise", "church", "mosqu[ée]e?", "synagogue", "temple"],
+    "science": ["science", "scientifique", "scientist", "physi(que|c)", " math", "informati(que|c)", "computer", "technolog(ie|y)", " astronom(ie|y)", "biolog(ie|y)", "chimie", "chemistry", "[ée]conom(ie|y)", "géolog(ie|y)", "m[ée]decine", "psycholog(ie|y)", "santé", "health", "sociolog(ie|y)", "zoolog(ie|y)"],
+    "seminar": ["seminar", "s[ée]minaire", " conf[ée]rence"],
+    "tour": ["tour", "visite"],
+    "workshop": ["workshop", "atelier"],
+    "free": ["free entrance", "gratuit", "entr[ée] libre"],
+    "fashion": ["fashion", "la mode", "v[êèe]tement"],
     "fair": ["fair", "foire"],
-    "videogame": ["gaming", "jeu vid", "jeux vid", "game", "games", "video game", "playstation", "xbox", "nintendo"],
-    "boardgame": ["boardgame", "jeu de société", "jeux de société", "jeu de plateau", "jeux de plateau", "jeu de cartes", "jeux de cartes", "jeu de rôle", "tarot", "belote"],
+    "videogame": ["gaming", "jeux? vid", "video game", "playstation", "xbox", "nintendo"],
+    "boardgame": ["boardgame", "jeux? de (soci[ée]t[ée]|plateau|cartes|r[ôo]le)", "tarot", "belote"],
 };
 
 /**
@@ -56,7 +56,7 @@ export function findCategories(...texts) {
     return Object.entries(categoriesKeywords)
         .filter(([category, keywords]) =>
             keywords.some(keyword =>
-                texts.some(text => text.includes(keyword))
+                texts.some(text => text.match(new RegExp(keyword)))
             )
         ).map(([category]) => category);
 }
