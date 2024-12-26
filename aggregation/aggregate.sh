577b803d3d570e63a667c12e58a8974b84fdf001
Ambi <ptlc.ptlc.ptlc@gmail.com>
2024-12-27 00:01:45 +0100

    aggregation: shorter message on success + use jq

--- aggregation/aggregate.sh
+++ aggregation/aggregate.sh
@@ -1,29 +1,19 @@
 #!/bin/bash
 
-# Usage: escapejson <texte>
-function escapejson() {
-    echo "$1" | sed 's/\\/\\\\/g;s/"/\\\"/g' | sed ':a;N;$!ba;s/\n/\\n/g'
-}
-
 # Usage: sendwebhook <title> <description> <color>
 function sendwebhook() {
     if [ -z "$AGGREGATION_WEBHOOK_URL" ]; then
         echo "No webhook URL provided, skipping notification"
         return
     fi
-    local title=$(escapejson "$1")
-    local description=$(escapejson "$2")
-    if ! [ -z "$description" ]; then
-        local description='```\n'"$description"'```'
+    if ! [ -z "$2" ]; then
+        local 2='```\n'"$2"'```'
     fi
+    local content=$(echo -e "$1\n$2" | jq --raw-input '.')
     curl -X POST \
          -H 'Content-Type: application/json' \
          -d '{
-               "embeds": [{
-                   "title": "'"$title"'",
-                   "description": "'"$description"'",
-                   "color": '"$3"'
-               }]
+               "content": '"$content"'
              }' \
          ${AGGREGATION_WEBHOOK_URL}
 }
@@ -39,8 +29,10 @@ echo "$logs"
 if [ $exitcode -eq 0 ]; then
     emojis=(🍏 🥬 💚 📗 🍀 🌱)
     emoji=${emojis[$RANDOM % ${#emojis[@]}]}
+    result=$(echo $(echo "$logs" | tr '\n' ' ') | grep -oP '\{[^\{]*\}$')
+    newevents=$(echo "$result" | jq '."db.inserted"')
     echo "$emoji $1 aggregation success"
-    sendwebhook "$emoji $1 aggregation success" "$logs" 56320
+    sendwebhook "$emoji $1 aggregation success" "$newevents new events" 56320
 else
     emojis=(🍎 ❤ 📕 🍅 🍒 🌶)
     emoji=${emojis[$RANDOM % ${#emojis[@]}]}
